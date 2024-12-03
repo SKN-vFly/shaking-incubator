bf87f45420894abf591f77a914c083619f6b5ff8
dawidokox1 <16292679+dawidokox1@users.noreply.github.com>
2024-12-03 14:50:31 +0100

    elp

--- shaking-incubator.ino
+++ shaking-incubator.ino
@@ -1,16 +1,28 @@
-const int out1pin = 12;
-const int out2pin = 13;
-const int out3pin = 10;
-const int out4pin = 11;
+#include<Arduino.h>
+
+const int set1forward = 12;
+const int set1back = 13;
+const int set2forward = 10;
+const int set2back = 11;
 bool play = true;
-const int DELAY = 10;
+const int DELAY = 1;
 unsigned long previousMillis = 0;
-unsigned long period = 1000;
+unsigned long period = 1100;
+unsigned long pulse_len = 100;
+unsigned long pulse_delay = 500;
+
+
+
+
+void pulse_full(int len);
+
+void pulse(int len, int delay, int pin);
+
 void setup() {
-  pinMode(out1pin, OUTPUT);
-  pinMode(out2pin, OUTPUT);
-  pinMode(out3pin, OUTPUT);
-  pinMode(out4pin, OUTPUT);
+  pinMode(set1forward, OUTPUT);
+  pinMode(set1back, OUTPUT);
+  pinMode(set2forward, OUTPUT);
+  pinMode(set2back, OUTPUT);
 
   Serial.begin(9600);
 }
@@ -23,13 +35,21 @@ void loop() {
     period = inp.toInt();
   }
   Serial.println(period);
-
+      pulse(pulse_len, pulse_delay, set1forward);
+      
+      pulse(pulse_len, pulse_delay, set2forward);
+      
+      pulse(pulse_len,pulse_delay, set1back);
+      
+      pulse(pulse_len, pulse_delay, set2back);
+      
   if (currentMillis - previousMillis >= period) {
-     pulse_full(period/2);
+     //pulse_full(period/2);
+     
      previousMillis = currentMillis;
   }
 }
-
+/*
 void pulse_full(int len){
     digitalWrite(out1pin, LOW);
     digitalWrite(out2pin, HIGH);
@@ -39,6 +59,7 @@ void pulse_full(int len){
     digitalWrite(out2pin, LOW);
     digitalWrite(out4pin, LOW);
     delay(DELAY);
+    
     digitalWrite(out1pin, HIGH);
     digitalWrite(out2pin, LOW);
     digitalWrite(out3pin, HIGH);
@@ -46,5 +67,13 @@ void pulse_full(int len){
     delay(len - DELAY);
     digitalWrite(out1pin, LOW);
     digitalWrite(out3pin, LOW);
+    
     delay(DELAY);    
+}
+*/
+void pulse(int len, int delay int pin){
+  digitalWrite(pin, HIGH);
+  delay(len);
+  digitalWrite(pin, LOW);
+  delay(delay)
 }
